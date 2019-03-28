80d6567dfb76da47a56727957bd221c534eead0c
Shohta Osaki <oskshtrustvl@gmail.com>
2019-03-28 11:27:27 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@
   get  '/help',    to: 'static_pages#help', as: 'helf'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
-  get  '/signup',  to: 'users#new', as: 'signup'
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
 end
